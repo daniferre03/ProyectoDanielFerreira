cc36499cbb36e42227ca986f2514085b9d10e376
PcBox <PcBox@DESKTOP-1KV4LV8>
2022-02-21 11:29:54 +0100

    Primer getter "getNombreEquipo"

--- src/DanielFerreiraFutbol.java
+++ src/DanielFerreiraFutbol.java
@@ -1,4 +1,13 @@
 
 public class DanielFerreiraFutbol {
-
+	
+	public String nombreEquipo;
+	public int nJugadores;
+	public String nombreJugador;
+	
+	public DanielFerreiraFutbol(String nombreEquipo, int nJugadores, String nombreJugador) {
+		this.nombreEquipo=nombreEquipo;
+		this.nJugadores=nJugadores;
+		this.nombreJugador=nombreJugador;
+	}
 }
